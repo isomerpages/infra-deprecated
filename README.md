c04e3dfb489e3665fae4518bbec8d99eb365b66d
Preston <prestonlimlianjie@gmail.com>
2018-10-23 16:00:59 +0800

    moved the test and deploy scripts into the travis.yml file, added more comments

--- README.md
+++ README.md
@@ -2,7 +2,12 @@
 
 #### How to use
 
-1. Make the relevant changes in the **terraform** folder
+1. Make the relevant changes in the **terraform** folder.
+
+Naming convention:
+**Folders** - website url in lowercase, with periods replaced by underscores. e.g. `isomer_gov_sg` for _isomer.gov.sg_
+
+**Files** - cloud provider concatenated with function in lower case, with periods replaced by underscores. e.g. `aws_cdn.tf` or `googlecloud_cloud_bucket.tf`
 
 2. Make sure that you are in the root directory, and run the following code:
 
--- run-terraform.sh
+++ run-terraform.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-if [ ! -d "run-dir" ]; then
+# Resets the directory where terraform will run, i.e. run-dir
+if [ ! -d "run-dir" ]; then # If the run-dir doesn't exist, create it
   	mkdir run-dir
-else
+else # Else, delete all .tf files
   	cd run-dir
   	rm *.tf
   	cd ..
 fi
+
+# =====================================================
+# Goes through all .tf files in the terraform directory, 
+# copies and renames them by concatenating the dirname with filename, 
+# and spits them into the run-dir.
+#
+# For example, the original file in the terraform folder, 
+# isomer_gov_sg/aws_cdn.tf will be renamed as isomer_gov_sg_aws_cdn.tf
+# =====================================================
 
 for DIR in terraform/*; do
 	DIRNAME=$(basename $DIR)
@@ -16,6 +26,8 @@ for DIR in terraform/*; do
 	done
 done
 
+# Move the terraform binary into the run-dir
 cp bin/terraform run-dir
 
+# Initialize terraform config
 cp init.tf run-dir
